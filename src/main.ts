fc17aa6067ac3abe75b9dfc3f57941720deef241
saraabdelsalam <105568705+saraabdelsalam@users.noreply.github.com>
2025-06-20 01:01:49 +0300

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // Enable CORS for Angular frontend
+  app.enableCors({
+    origin: 'http://localhost:4200', // allow Angular
+    credentials: true, // if using cookies or auth headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
